3dcd04a8e40f2fd6d9ea0fa424350e0e52793fe5
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-06-24 16:17:23 -0700

    poly update

--- Python Projects/polymorphism.py
+++ Python Projects/polymorphism.py
@@ -9,7 +9,7 @@ class User:
         e_name = input("Enter your name: ")
         e_email = input("Enter your email: ")
         e_password = input("Enter your password: ")
-        if (e_email == self.email and entry_password == self.password):#if email and password match, prints next statement
+        if (e_email == self.email and e_password == self.password):#if email and password match, prints next statement
             print("Welcome back, {}!".format(e_name))
         else:
             print("The password or email is incorrect.")#if email or password dont match, prints this statement
@@ -32,6 +32,26 @@ class Employee(User):
             print("Welcome back, {}!".format(e_name))
         else:
             print("The pin or email was incorrect")
+
+
+#child class Social
+class Social(User):
+    account = "Chandler_Bing"
+    followers = "5"
+    passcode = "232435127"
+#Same method in the parent class 'User'
+#the difference is that instead of using e_password, its using passcode and
+
+    def loginInfo(self):
+        e_account = input("Enter your account name: ")
+        e_email = input("Enter your email: ")
+        e_passcode = input("Enter your passcode: ")
+        if (e_account == self.account and e_passcode == self.passcode):
+            print("Thanks for logging in, {}!".format(e_account))
+        else:
+            print("Your login information was incorrect, please try again!")
+
+
 #this below code invokes the methods inside each class for 'User' and 'Employee'
 
 customer = User()
@@ -39,3 +59,6 @@ customer.loginInfo()
 
 manager = Employee()
 manager.loginInfo()
+
+login = Social()
+login.loginInfo()
